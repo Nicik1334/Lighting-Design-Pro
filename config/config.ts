5f607bba4947eb368dbac3656a064795124eced8
Yin <1940637041@qq.com>
2023-03-02 10:08:53 +0800

    fix: 修改圆角

--- config/config.ts
+++ config/config.ts
@@ -30,8 +30,10 @@ export default defineConfig({
   theme: {
     // 如果不想要 configProvide 动态设置主题需要把这个设置为 default
     // 只有设置为 variable， 才能使用 configProvide 动态设置主色调
-    // https://ant.design/docs/react/customize-theme-variable-cn
+    // https://4x.ant.design/docs/react/customize-theme-variable-cn
     'root-entry-name': 'variable',
+    'border-radius-base': '6px',
+    'border-radius': '6px',
   },
   // esbuild is father build tools
   // https://umijs.org/plugins/plugin-esbuild
